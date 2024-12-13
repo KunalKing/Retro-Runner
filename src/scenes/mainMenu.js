3abdf8bc1f61aef170ca10a1a696169a40e50790
KunalKing <Kunal.kunalprasad@outlook.com>
2024-12-13 16:03:08 +0530

    Background and assets

---
+++ src/scenes/mainMenu.js
@@ -0,0 +1,10 @@
+import k from "../kaplayCtx";
+
+
+export default function mainMenu(){
+    if(!k.getData("best-score")) k.setData("best-score", 0);
+    k.onButtonPress("jump", () => k.go("game"));
+
+    
+    
+};
