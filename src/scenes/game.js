68911c726c88f777bed82c920e13b480b1705152
KunalKing <Kunal.kunalprasad@outlook.com>
2024-12-14 22:59:45 +0530

    Characters Controls Development

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,3 +1,4 @@
+import { makeSonic } from "../entities/sonic";
 import k from "../kaplayCtx";
 
 export default function game(){
@@ -20,10 +21,22 @@ export default function game(){
      k.add([k.sprite("platforms"), k.pos(platformWidth, 450), k.scale(4)]),
    ];
 
+   const sonic = makeSonic(k.vec2(200, 745));
+   sonic.setControls();
+   sonic.setEvents(); 
+
    let gameSpeed = 300;
    k.loop(1, () => {
     gameSpeed += 50;
    });
+
+   k.add([
+     k.rect(1920, 300),
+     k.opacity(0),
+     k.area(),
+     k.pos(0, 832),
+     k.body({isStatic: true}),
+   ]);
 
    k.onUpdate(() => {
      if(bgPieces[1].pos.x < 0){
